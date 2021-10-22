ee56c27f6259b0d64cabcd0f3d5fa827d72e3c18
Ivan Makovetskiy <ivan.makovetskiy@yandex.ru>
2021-10-22 14:43:03 +0300

    1-7 done

--- 1/1-7.scm
+++ 1/1-7.scm
@@ -1,22 +1,21 @@
-(define (new-if predicate then-clause else-clause)
-  (cond (predicate then-clause)
-	(else else-clause)))
-
-(define (sqrt-iter guess x)
-  (new-if (good-enough? guess x)
-	  guess 
-	  (sqrt-iter (improve guess x)
-		     x)))
-
-(define (improve guess x)
-  (average guess (/ x guess)))
+(define (square x)
+  (* x x))
 
 (define (average x y)
   (/ (+ x y) 2))
 
-(define (good-enough? guess x)
-  (< (abs (- (improve guess x) guess)) 
-     (* guess 0.001))
+(define (improve guess x)
+  (average guess (/ x guess)))
 
-(define (sqrt x)
-  (sqrt-iter 1.0 x))
+(define (better-good-enough? prev-guess guess)
+  (< (abs (- guess prev-guess))
+     0.00001))
+
+(define (better-sqrt-iter prev-guess guess x)
+  (if (better-good-enough? prev-guess guess)
+      guess
+      (better-sqrt-iter guess
+                        (improve guess x)
+                        x)))
+(define (better-sqrt x)
+  (better-sqrt-iter 0 1.0 x))
